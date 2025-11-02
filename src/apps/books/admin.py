509a76f088a0538ae736fd045f29393cd66fce24
Ehsan Akbari <ehsanakbari.dev@gmail.com>
2025-11-02 07:46:01 -0800

    feat: register Book model in admin with optimized interface

--- src/apps/books/admin.py
+++ src/apps/books/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 from .models import Book
-admin.site.register(Book)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'isbn', 'publication_year', 'created_at']
+    list_filter = ['author', 'publication_year', 'created_at']
+    search_fields = ['title', 'author', 'isbn']
+    readonly_fields = ['created_at', 'updated_at']
+    ordering = ['-created_at']
+    date_hierarchy = 'created_at'
+
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('title', 'author', 'isbn')
+        }),
+        ('Additional Information', {
+            'fields': ('publication_year',)
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
